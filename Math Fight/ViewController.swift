27c46e0e587d5a39b1b980f95280a3d6ecb277aa
Harry Yuan <jianma@jians-MacBook-Pro.local>
2017-07-08 20:01:05 -0700

    Initial commit - add all the files
    Very lengthy description. This added all the files generated from create-react-native-app.

---
+++ Math Fight/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Math Fight
+//
+//  Created by jian ma on 6/11/17.
+//  Copyright © 2017 appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
